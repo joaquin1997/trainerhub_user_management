8ff3753fb992b6690f30ef642ed7a744bb202bf8
joaqu <jjgarcia@bilbomatica.es>
2024-09-15 18:06:02 +0200

    Added skeleton

---
+++ src/main/java/com/trainerhub/user_management/TrainerhubUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.trainerhub.user_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerhubUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerhubUserManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trainerhub/user_management/TrainerhubUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainerhub.user_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerhubUserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
